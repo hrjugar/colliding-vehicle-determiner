0b19510c20f7985db6f0f5559cd7607dd6075f20
hrjugar <hernanjugar123@gmail.com>
2024-01-17 16:54:44 +0800

    Made seek bar clickable and seek bar handle draggable

--- src/components/modals/EditVideoModal/IdentifyVehiclesPanel/IdentifyVehiclesVideoPlayer.tsx
+++ src/components/modals/EditVideoModal/IdentifyVehiclesPanel/IdentifyVehiclesVideoPlayer.tsx
@@ -1,13 +1,9 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import useIdentifyVehiclesPanelStore from './store';
 import { useShallow } from 'zustand/react/shallow';
-import { convertSecondsAndMillisecondsToString, convertSecondsToMinutes } from '@/globals/utils';
+import { convertSecondsToMinutes } from '@/globals/utils';
 
-interface IdentifyVehiclesVideoProps {
-  // Add any props you need for the component
-}
-
-const IdentifyVehiclesVideoPlayer: React.FC<IdentifyVehiclesVideoProps> = () => {
+const IdentifyVehiclesVideoPlayer: React.FC = () => {
   const [
     getSelectedObject,
     duration,
@@ -31,6 +27,7 @@ const IdentifyVehiclesVideoPlayer: React.FC<IdentifyVehiclesVideoProps> = () => 
   )
 
   const videoRef = useRef<HTMLVideoElement>(null);
+  const seekBarMarkerRef = useRef<HTMLDivElement>(null);
   const selectedObject = getSelectedObject();
 
   const handleVideoSkip = (isBackwards : boolean) => {
@@ -63,6 +60,55 @@ const IdentifyVehiclesVideoPlayer: React.FC<IdentifyVehiclesVideoProps> = () => 
       setDuration(videoRef.current.duration);
     }
   };
+
+  const handleSliderClick = (e: React.MouseEvent) => {
+    if (videoRef.current) {
+      const rect = e.currentTarget.getBoundingClientRect();
+      const x = e.clientX - rect.left;
+      const leftPercentage = x / rect.width;
+      videoRef.current.currentTime = videoRef.current.duration * leftPercentage;
+      setTimePercentage(leftPercentage * 100);
+    }
+  };
+
+  const onPointerDown = (e: React.PointerEvent) => {
+    e.preventDefault();
+    document.addEventListener('pointermove', onPointerMove);
+    document.addEventListener('pointerup', onPointerUp);
+  };
+
+  const onPointerUp = (e: PointerEvent) => {
+    e.preventDefault();
+    document.removeEventListener('pointermove', onPointerMove);
+    document.removeEventListener('pointerup', onPointerUp);
+  };
+
+  const onPointerMove = (e: PointerEvent) => {
+    e.preventDefault();
+    if (videoRef.current) {
+      const parentRect = seekBarMarkerRef.current?.parentElement!.getBoundingClientRect()!;
+      const newPosition = (e.clientX - parentRect.left);
+      const newPositionPercentage = newPosition / parentRect.width;
+
+      if (newPositionPercentage < 0) {
+        videoRef.current.currentTime = 0;
+        setTimePercentage(0);
+      } else if (newPositionPercentage > 1) {
+        videoRef.current.currentTime = videoRef.current.duration;
+        setTimePercentage(100);
+      } else {
+        videoRef.current.currentTime = duration * newPositionPercentage;
+        setTimePercentage(newPositionPercentage * 100);
+      }
+    }
+  };
+
+  useEffect(() => {
+    return () => {
+      window.removeEventListener('pointerup', onPointerUp);
+      window.removeEventListener('pointermove', onPointerMove);
+    }
+  }, [])
 
   return (
     <div className='card w-full h-full flex flex-col'>
@@ -134,14 +180,19 @@ const IdentifyVehiclesVideoPlayer: React.FC<IdentifyVehiclesVideoProps> = () => 
         {videoRef.current && duration >= 0 ? (
           <p className='text-xs font-medium'>{convertSecondsToMinutes(videoRef.current.currentTime)}/{convertSecondsToMinutes(videoRef.current.duration)}</p>
         ) : null}
-        <div className='relative bg-gray-200 w-full h-2 rounded-full flex flex-row items-center'>
+        <div 
+          className='relative bg-gray-200 w-full h-2 rounded-full flex flex-row items-center'
+          onClick={handleSliderClick}
+        >
           <div 
             className='absolute h-2 rounded-full bg-color-primary-active/50'
             style={{ width: `${timePercentage}%` }}
           />
           <div 
-            className='absolute w-4 h-4 rounded-full bg-color-primary -translate-x-1/2'
+            className='absolute w-4 h-4 rounded-full bg-color-primary -translate-x-1/2 cursor-pointer'
+            ref={seekBarMarkerRef}
             style={{ left: `${timePercentage}%` }}
+            onPointerDown={onPointerDown}
           />
         </div>
       </div>
