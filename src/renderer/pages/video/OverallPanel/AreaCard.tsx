db8425260836f72eb1a29b5326b21302b30d5f5b
hrjugar <hernanjugar123@gmail.com>
2024-02-29 13:32:11 +0800

    Adjust video page colliding vehicle data UI

--- src/renderer/pages/video/OverallPanel/AreaCard.tsx
+++ src/renderer/pages/video/OverallPanel/AreaCard.tsx
@@ -41,7 +41,6 @@ const AreaCard: React.FC<AreaCardProps> = ({
   let areaBox: BoundingBox | BoundingBoxWithIdAndProbability;
   let areaName: string;
   let boxClasses: string;
-  let isCollidingVehicle = false;
 
   switch (cardAreaType) {
     case AreaType.Accident:
@@ -53,88 +52,54 @@ const AreaCard: React.FC<AreaCardProps> = ({
       areaBox = video.accidentFrameVehicleOne!;
       areaName = "Vehicle One";
       boxClasses = "border-green-400 bg-green-400";
-      
-      if (video.accidentFrameVehicleOne && video.accidentFrameVehicleTwo) {
-        isCollidingVehicle = video.accidentFrameVehicleOne!.probability > video.accidentFrameVehicleTwo!.probability;
-      }
-    break;
+      break;
     case AreaType.VehicleTwo:
       areaBox = video.accidentFrameVehicleTwo!;
       areaName = "Vehicle Two";
       boxClasses = "border-blue-400 bg-blue-400";
-
-      if (video.accidentFrameVehicleOne && video.accidentFrameVehicleTwo) {
-        isCollidingVehicle = video.accidentFrameVehicleTwo!.probability > video.accidentFrameVehicleOne!.probability;
-      }
       break;
   }
 
+  const isHovered = currentHoveredArea === cardAreaType;
+
   return (
     <div 
-      className="card group flex flex-row"
+      className={`card group flex flex-row`}
       onMouseOver={(e) => {e.stopPropagation(); setHoveredArea(cardAreaType); }}
       onMouseLeave={() => {
-        if (currentHoveredArea === cardAreaType) {
+        if (isHovered) {
           setHoveredArea(null);
         }
       }}
     >
       <div className="flex flex-col items-center transition-all">
-        <div className="bg-black overflow-hidden">
+        <div className={`bg-black overflow-hidden`}>
           <img 
             src={`filehandler://frame//${video.id}//${video.accidentFrame}`}
-            className={`${currentHoveredArea === cardAreaType ? "h-[calc(16vh_+_38px)]" : "h-[16vh]"} w-full object-contain transition-all`}
+            className={`${isHovered ? "h-[calc(16vh_+_38px)] opacity-75" : "h-[16vh]"} w-full object-scale-down transition-all`}
             style={ getCroppedImageStyle(areaBox) }
           />
         </div>
-        <div className={`card-header border-0 ${currentHoveredArea === cardAreaType ? "h-0 gap-0 p-0" : "h-[38px] gap-2"} justify-start transition-all`}>
-          <div className={`${currentHoveredArea === cardAreaType ? "w-0 h-0 border-0" : "w-3 h-3 border-2"} ${boxClasses} transition-all`}/>
-          <p className={`whitespace-nowrap text-left font-semibold ${currentHoveredArea === cardAreaType ? "text-[0px]" : "text-sm"} flex flex-row`}>{areaName}</p>
-          {isCollidingVehicle ? (
-            <svg 
-              width="64" 
-              height="64" 
-              viewBox="0 0 64 64" 
-              xmlns="http://www.w3.org/2000/svg"
-              className={`text-gray-500 ${currentHoveredArea === cardAreaType ? "w-0 h-0" : "w-4 h-4"} transition-all`}
-            >
-              <path 
-                d="M26.6667 45.3334L13.3333 32L17.0933 28.2134L26.6667 37.7867L46.9067 17.5467L50.6667 21.3334M32 5.33337C28.4981 5.33337 25.0305 6.02313 21.7951 7.36325C18.5598 8.70338 15.6201 10.6676 13.1438 13.1439C8.14286 18.1448 5.33334 24.9276 5.33334 32C5.33334 39.0725 8.14286 45.8553 13.1438 50.8562C15.6201 53.3325 18.5598 55.2967 21.7951 56.6368C25.0305 57.977 28.4981 58.6667 32 58.6667C39.0725 58.6667 45.8552 55.8572 50.8562 50.8562C55.8572 45.8553 58.6667 39.0725 58.6667 32C58.6667 28.4981 57.9769 25.0305 56.6368 21.7951C55.2967 18.5598 53.3324 15.6201 50.8562 13.1439C48.38 10.6676 45.4403 8.70338 42.2049 7.36325C38.9696 6.02313 35.5019 5.33337 32 5.33337Z"
-                className='fill-current'
-              />
-            </svg>        
-          ) : null}
+        <div className={`card-header border-0 ${isHovered ? "h-0 gap-0 p-0" : "h-[38px] gap-2"} justify-start transition-all`}>
+          <div className={`${isHovered ? "w-0 h-0 border-0" : "w-3 h-3 border-2"} ${boxClasses} transition-all`}/>
+          <p className={`whitespace-nowrap text-left font-semibold ${isHovered ? "text-[0px]" : "text-sm"} flex flex-row`}>{areaName}</p>
         </div>
       </div>
 
-      <div className={`flex flex-col ${currentHoveredArea === cardAreaType ? "gap-2 text-sm" : "gap-0 text-[0px]"} text-left font-semibold transition-all`}>
-        <div className={`flex flex-row items-center ${currentHoveredArea === cardAreaType ? "px-4 py-2 gap-2" : "p-0 gap-0"} border-b-2 border-gray-300`}>
-          <div className={`${currentHoveredArea === cardAreaType ? "w-3 h-3 border-2" : "border-0"} ${boxClasses} transition-all`}/>
+      <div className={`flex flex-col ${isHovered ? "gap-2 text-sm" : "gap-0 text-[0px]"} text-left font-semibold transition-all`}>
+        <div className={`flex flex-row items-center ${isHovered ? "px-4 py-2 gap-2" : "p-0 gap-0"} border-b-2 border-gray-300`}>
+          <div className={`${isHovered ? "w-3 h-3 border-2" : "border-0"} ${boxClasses} transition-all`}/>
           <p className="whitespace-nowrap text-left">{areaName}</p>
-          {isCollidingVehicle ? (
-            <svg 
-            width="64" 
-            height="64" 
-            viewBox="0 0 64 64" 
-            xmlns="http://www.w3.org/2000/svg"
-            className={`text-gray-500 ${currentHoveredArea === cardAreaType ? "w-4 h-4" : "w-0 h-0"} transition-all`}
-          >
-            <path 
-              d="M26.6667 45.3334L13.3333 32L17.0933 28.2134L26.6667 37.7867L46.9067 17.5467L50.6667 21.3334M32 5.33337C28.4981 5.33337 25.0305 6.02313 21.7951 7.36325C18.5598 8.70338 15.6201 10.6676 13.1438 13.1439C8.14286 18.1448 5.33334 24.9276 5.33334 32C5.33334 39.0725 8.14286 45.8553 13.1438 50.8562C15.6201 53.3325 18.5598 55.2967 21.7951 56.6368C25.0305 57.977 28.4981 58.6667 32 58.6667C39.0725 58.6667 45.8552 55.8572 50.8562 50.8562C55.8572 45.8553 58.6667 39.0725 58.6667 32C58.6667 28.4981 57.9769 25.0305 56.6368 21.7951C55.2967 18.5598 53.3324 15.6201 50.8562 13.1439C48.38 10.6676 45.4403 8.70338 42.2049 7.36325C38.9696 6.02313 35.5019 5.33337 32 5.33337Z"
-              className='fill-current'
-            />
-          </svg>     
-          ) : null}
         </div>
 
-        <div className={`${currentHoveredArea === cardAreaType ? "px-4 gap-x-4 gap-y-2" : "p-0 gap-0"} grid grid-rows-3 grid-cols-2`}>
-          <p className={`flex flex-row items-center ${currentHoveredArea === cardAreaType ? "gap-2" : "gap-0"}`}><span className="text-gray-400">X</span> <span className="font-medium">{Math.round(areaBox.x)}</span></p>
-          <p className={`flex flex-row items-center ${currentHoveredArea === cardAreaType ? "gap-2" : "gap-0"}`}><span className="text-gray-400">Y</span> <span className="font-medium">{Math.round(areaBox.y)}</span></p>
-          <p className={`place-self-start flex flex-row items-center ${currentHoveredArea === cardAreaType ? "gap-2" : "gap-0"}`}><span className="text-gray-400">W</span> <span className="font-medium">{Math.round(areaBox.w)}</span></p>
-          <p className={`place-self-start flex flex-row items-center ${currentHoveredArea === cardAreaType ? "gap-2" : "gap-0"}`}><span className="text-gray-400">H</span> <span className="font-medium">{Math.round(areaBox.h)}</span></p>
+        <div className={`${isHovered ? "px-4 gap-x-4 gap-y-2" : "p-0 gap-0"} grid grid-rows-3 grid-cols-2`}>
+          <p className={`flex flex-row items-center ${isHovered ? "gap-2" : "gap-0"}`}><span className="text-gray-400">X</span> <span className="font-medium">{Math.round(areaBox.x)}</span></p>
+          <p className={`flex flex-row items-center ${isHovered ? "gap-2" : "gap-0"}`}><span className="text-gray-400">Y</span> <span className="font-medium">{Math.round(areaBox.y)}</span></p>
+          <p className={`place-self-start flex flex-row items-center ${isHovered ? "gap-2" : "gap-0"}`}><span className="text-gray-400">W</span> <span className="font-medium">{Math.round(areaBox.w)}</span></p>
+          <p className={`place-self-start flex flex-row items-center ${isHovered ? "gap-2" : "gap-0"}`}><span className="text-gray-400">H</span> <span className="font-medium">{Math.round(areaBox.h)}</span></p>
 
           {cardAreaType !== AreaType.Accident && areaBox && 'probability' in areaBox ? (
-            <p className={`place-self-start flex flex-row items-center ${currentHoveredArea === cardAreaType ? "gap-2" : "gap-0"}`}><span className="text-gray-400">%</span> <span className="font-medium">{areaBox.probability.toFixed(4)}</span></p>
+            <p className={`place-self-start flex flex-row items-center ${isHovered ? "gap-2" : "gap-0"}`}><span className="text-gray-400">%</span> <span className="font-medium">{(areaBox.probability * 100).toFixed(2)}</span></p>
           ) : null}
         </div>
       </div>
--- src/renderer/pages/video/OverallPanel/index.tsx
+++ src/renderer/pages/video/OverallPanel/index.tsx
@@ -39,11 +39,13 @@ const getFrameDarkenerStyle = (video: VideoData, hoveredArea: AreaType | null): 
 const OverallPanel: React.FC = () => {
   const video = useLoaderData() as VideoData;
 
-  let collidingVehicle;
+  let collidingVehicle = "NONE";
+  let vehicleOneProbabilityStr = "0%";
+  let vehicleTwoProbabilityStr = "0%";
   if (video.accidentFrameVehicleOne && video.accidentFrameVehicleTwo) {
     collidingVehicle = video.accidentFrameVehicleOne.probability > video.accidentFrameVehicleTwo.probability ? "Vehicle One" : "Vehicle Two";
-  } else {
-    collidingVehicle = "None";
+    vehicleOneProbabilityStr = `${(video.accidentFrameVehicleOne.probability * 100).toFixed(2)}%`;
+    vehicleTwoProbabilityStr = `${(video.accidentFrameVehicleTwo.probability * 100).toFixed(2)}%`;
   }
 
   const imageRef = useRef<HTMLImageElement>(null);
@@ -73,107 +75,203 @@ const OverallPanel: React.FC = () => {
         resizeObserver.unobserve(imageRef.current);
       }
     }
-  }, [imageRef])
+  }, [imageRef]);
+
+  useEffect(() => {
+    // Create a new style element
+    const styleElement = document.createElement('style');
+  
+    // Define the keyframes with the dynamic end value
+    styleElement.textContent = `
+      @keyframes expandWidth {
+        0% { width: 50%; }
+        100% { width: ${vehicleOneProbabilityStr}; }
+      }
+    `;
+  
+    // Append the style element to the document head
+    document.head.appendChild(styleElement);
+  
+    // Clean up function to remove the style element when the component unmounts
+    return () => {
+      document.head.removeChild(styleElement);
+    };
+  }, [vehicleOneProbabilityStr]);
 
   return (
-    <Tab.Panel className="flex flex-col gap-4">
+    <Tab.Panel className="flex flex-col gap-4 pt-2">
       {video.accidentFrame ? (
-        <section className="w-full h-[calc(100vh_-_155px)]  flex flex-col">
-          <div className="flex flex-col w-full flex-grow gap-4">
-            <h2 className="text-2xl font-medium text-color-primary">Colliding Vehicle: <span className="font-bold">{collidingVehicle}</span></h2>
+        <>
+          <section className="w-full h-[calc(100vh_-_64px_-_1rem)] flex flex-col gap-4">
+            <div className="flex flex-col px-8">
+              <p className="text-xl">Colliding Vehicle: <span className="font-semibold uppercase">{collidingVehicle}</span></p>
+              {video.accidentFrameVehicleOne && video.accidentFrameVehicleTwo ? (
+                <div className="flex flex-row items-center pt-2 whitespace-nowrap gap-4">
+                  <p className={`${video.accidentFrameVehicleOne.probability > video.accidentFrameVehicleTwo.probability ? 'font-bold' : ''}`}>Vehicle One</p>
+                  <div className="relative w-full flex flex-row h-10">
+                    <div 
+                      className="bg-green-400 h-full flex justify-start items-center pl-2 animate-expand-width overflow-hidden"
+                      style={{
+                        // width: vehicleOneProbabilityStr,
+                        animation: 'expandWidth 1s forwards',
+                      }}
+                    >
+                      <p className={`${video.accidentFrameVehicleOne.probability > video.accidentFrameVehicleTwo.probability ? 'text-black font-semibold' : 'text-black/50 font-medium'}`}>{vehicleOneProbabilityStr}</p>
+                    </div>
+                    <div 
+                      className="bg-blue-400 flex-1 h-full flex justify-end items-center pr-2"
+                    >
+                      <p className={`${video.accidentFrameVehicleOne.probability < video.accidentFrameVehicleTwo.probability ? 'text-black font-semibold' : 'text-black/50 font-medium'}`}>{vehicleTwoProbabilityStr}</p>
+                    </div>
+                  </div>
+                  <p className={`${video.accidentFrameVehicleOne.probability > video.accidentFrameVehicleTwo.probability ? '' : 'font-bold'}`}>Vehicle Two</p>
+                </div>                
+              ): null}
+            </div>
 
-            <div className="relative w-full flex-grow flex justify-center items-center h-0">
-              <img 
-                src={`filehandler://frame//${video.id}//${video.accidentFrame}`} 
-                className="object-contain max-w-full h-full"
-                ref={imageRef}
-                onLoad={updateBoundingBoxAreaSize}
-              />
+            <div className="flex flex-col w-full h-full flex-grow gap-2">
+              <div className="w-full flex-grow flex flex-col gap-2">    
+                <div className="relative w-full flex-grow flex flex-col justify-center items-center h-0 bg-black">
+                  <img 
+                    src={`filehandler://frame//${video.id}//${video.accidentFrame}`} 
+                    className="object-contain max-w-full h-full"
+                    ref={imageRef}
+                    onLoad={updateBoundingBoxAreaSize}
+                  />
 
-              <div
-                ref={boundingBoxAreaRef}
-                className="absolute overflow-hidden"
-              >
-                <div 
-                  className="absolute" 
-                  style={ getFrameDarkenerStyle(video, hoveredArea) }
-                />
-
-                {video.accidentArea ? (
-                  <>
-                    <AreaFrameBoundingBox 
-                      boxAreaType={AreaType.Accident}
-                      currentHoveredArea={hoveredArea}
-                      setHoveredArea={setHoveredArea}
-                      video={video}
+                  <div
+                    ref={boundingBoxAreaRef}
+                    className="absolute overflow-hidden"
+                  >
+                    <div 
+                      className="absolute" 
+                      style={ getFrameDarkenerStyle(video, hoveredArea) }
                     />
 
-                    {video.accidentFrameVehicleOne ? (
-                      <AreaFrameBoundingBox 
-                        boxAreaType={AreaType.VehicleOne}
-                        currentHoveredArea={hoveredArea}
-                        setHoveredArea={setHoveredArea}
-                        video={video}
-                      />         
-                    ) : null}
+                    {video.accidentArea ? (
+                      <>
+                        <AreaFrameBoundingBox 
+                          boxAreaType={AreaType.Accident}
+                          currentHoveredArea={hoveredArea}
+                          setHoveredArea={setHoveredArea}
+                          video={video}
+                        />
 
-                    {video.accidentFrameVehicleTwo ? (
-                      <AreaFrameBoundingBox 
-                        boxAreaType={AreaType.VehicleTwo}
-                        currentHoveredArea={hoveredArea}
-                        setHoveredArea={setHoveredArea}
-                        video={video}
-                      />                        
+                        {video.accidentFrameVehicleOne ? (
+                          <AreaFrameBoundingBox 
+                            boxAreaType={AreaType.VehicleOne}
+                            currentHoveredArea={hoveredArea}
+                            setHoveredArea={setHoveredArea}
+                            video={video}
+                          />         
+                        ) : null}
+
+                        {video.accidentFrameVehicleTwo ? (
+                          <AreaFrameBoundingBox 
+                            boxAreaType={AreaType.VehicleTwo}
+                            currentHoveredArea={hoveredArea}
+                            setHoveredArea={setHoveredArea}
+                            video={video}
+                          />                        
+                        ) : null}
+                      </>
                     ) : null}
-                  </>
+                    {/* <div 
+                      className="absolute border-2 border-red-400"
+                      style={
+                        left: `${(video.accidentArea!.xn - video.accidentArea!.wn / 2) * 100}%`,
+                        top: `${(video.accidentArea!.yn - item.hn / 2) * 100}%`,
+                        width: `${(item.wn) * 100}%`,
+                        height: `${(item.hn) * 100}%`,
+                        zIndex: Math.round(item.confidence * 100),
+                        transformOrigin: 'top left',                  
+                      }
+                    /> */}
+                  </div>
+                  
+                </div>
+              </div>
+
+              <div className="flex flex-row gap-4 px-4 py-4 justify-center max-w-full">
+                <AreaCard 
+                  cardAreaType={AreaType.Accident}
+                  currentHoveredArea={hoveredArea}
+                  setHoveredArea={setHoveredArea}
+                  video={video}
+                />
+
+                {video.accidentFrameVehicleOne ? (
+                  <AreaCard 
+                    cardAreaType={AreaType.VehicleOne}
+                    currentHoveredArea={hoveredArea}
+                    setHoveredArea={setHoveredArea}
+                    video={video}
+                  />
                 ) : null}
-                {/* <div 
-                  className="absolute border-2 border-red-400"
-                  style={
-                    left: `${(video.accidentArea!.xn - video.accidentArea!.wn / 2) * 100}%`,
-                    top: `${(video.accidentArea!.yn - item.hn / 2) * 100}%`,
-                    width: `${(item.wn) * 100}%`,
-                    height: `${(item.hn) * 100}%`,
-                    zIndex: Math.round(item.confidence * 100),
-                    transformOrigin: 'top left',                  
-                  }
-                /> */}
+
+                {video.accidentFrameVehicleTwo ? (
+                  <AreaCard 
+                    cardAreaType={AreaType.VehicleTwo}
+                    currentHoveredArea={hoveredArea}
+                    setHoveredArea={setHoveredArea}
+                    video={video}
+                  />
+                ) : null}                   
               </div>
             </div>
 
-            <div className="flex flex-row gap-4 px-1 justify-center max-w-full">
-              <AreaCard 
-                cardAreaType={AreaType.Accident}
-                currentHoveredArea={hoveredArea}
-                setHoveredArea={setHoveredArea}
-                video={video}
-              />
-
-              {video.accidentFrameVehicleOne ? (
-                <AreaCard 
-                  cardAreaType={AreaType.VehicleOne}
-                  currentHoveredArea={hoveredArea}
-                  setHoveredArea={setHoveredArea}
-                  video={video}
-                />
-              ) : null}
-
-              {video.accidentFrameVehicleTwo ? (
-                <AreaCard 
-                  cardAreaType={AreaType.VehicleTwo}
-                  currentHoveredArea={hoveredArea}
-                  setHoveredArea={setHoveredArea}
-                  video={video}
-                />
-              ) : null}                   
-            </div>
-          </div>
-        </section>
+          </section>                     
+        </>
       ) : (
-        <h2 className="text-2xl text-left">Video has no accident frames.</h2>
+        <h2 className=" pl-8 text-2xl text-left">Video has no accident frames.</h2>
       )}
     </Tab.Panel>
   )
 };
+
+// const OverallPanel: React.FC = () => {
+//   const video = useLoaderData() as VideoData;
+
+//   let collidingVehicle;
+//   if (video.accidentFrameVehicleOne && video.accidentFrameVehicleTwo) {
+//     collidingVehicle = video.accidentFrameVehicleOne.probability > video.accidentFrameVehicleTwo.probability ? "Vehicle One" : "Vehicle Two";
+//   } else {
+//     collidingVehicle = "None";
+//   }
+  
+//   return (
+//     <Tab.Panel className="flex flex-col gap-4 pl-8 pr-4 pt-2">
+//       {video.accidentFrame ? (
+//         <div className="flex flex-col gap-4">
+//           <div className="bg-black h-full flex justify-start items-center rounded-xl overflow-hidden">
+//             <img 
+//               src={`filehandler://frame//${video.id}//${video.accidentFrame}`} 
+//               className="object-scale-down w-full h-full max-h-[calc(100vh_-_20rem)]"          
+//             />
+//           </div>
+
+//           <div className="flex flex-row gap-4">
+//             <div className="row-span-2 flex flex-col card">
+//               <div className="bg-black overflow-hidden">
+//                 <img 
+//                   src={`filehandler://frame//${video.id}//${video.accidentFrame}`}
+//                   className="object-contain w-full h-full max-h-[calc(33vh_-_80px)]"
+//                 />
+//               </div>
+
+//               <div className="flex flex-col px-4 py-2">
+//                 <p className="font-semibold text-left">Accident Area</p>
+//               </div>
+//             </div>
+//           </div>
+
+//           <div></div>
+//         </div>
+//       ) : (
+//         <h2 className=" pl-8 text-2xl text-left">Video has no accident frames.</h2>
+//       )}
+//     </Tab.Panel>
+//   );
+// }
 
 export default OverallPanel;
